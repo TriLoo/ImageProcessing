559f906dc38bddf80cbd16d9a3a210fe4f0dca82
smh <song_mh@yeah.net>
2017-10-26 22:37:02 +0800

    fixed bugs

--- PCA_Python/main.py
+++ PCA_Python/main.py
@@ -1,58 +1,102 @@
 #encoding=UTF-8
-
-
-#from numpy import * as np    # only import some functions and args that not begin with underline('_')
-import numpy as np
+#import cv2
+from __future__ import print_function
 from PIL import Image
-import matplotlib.pyplot as plt
+import numpy as np
+import os, platform
+#from matplotlib import pyplot as plt
 
-def zeroMean(dataMat):
+def ImageRead(path, name):
     '''
-    Get the zero mean matrix
-    :param dataMat:
+    Read a image.
+
+    :param path:
+    :param name:
     :return:
     '''
-    meanVal = np.mean(dataMat, axis=0)   # one feature per column, get a row vector
-    print(meanVal.size)
-    newData = dataMat - meanVal          # Broadcast
-    return newData, meanVal
+    try:
+        img = Image.open(os.path.join(path, name)).convert('L')
+    except IOError:
+        print('Read Image failed ...')
+    else:
+        print(img.format, img.size, img.mode)
+    return img
 
-def pca(dataMat, n):
+class myPCA:
     '''
-    Top PCA function.
-    :param dataMat:
-    :param n:
-    :return:
+    This is a class about PCA based on Python.
     '''
-    newData, meanVal = zeroMean(dataMat)
-    covMat = np.cov(newData, rowvar=0)      # rowvar : one sample per line, return ndarray
+    def __init__(self, pre = 0.90):
+        self.__precison = pre
 
-    eigVals, eigVects = np.linalg.eig(np.mat(covMat))        # calculate the eig-vectors and eig-values
-    eigValIndice = np.argsort(eigVals)                       # 对特征值排序，从小到大
-    n_eigValIndice = eigValIndice[-1:-(n+1):-1]              # 最大的n个特征值的下标
+    def __zeroMean(self, dataMat):
+        means = np.mean(dataMat, axis=0)           # the average value of each feature, axis = 0 ==> per column
+        print(means.size)            # for test
+        # return a 2 * 1 tuple
+        return dataMat - means, means              # including a hidden broadcast operation
 
-    n_eigVect = eigVects[:, n_eigValIndice]
-    lowDDataMat = newData * n_eigVect                        # 低维特征空间的数据
-    reconMat = (lowDDataMat * n_eigVect.T) + meanVal
-    return lowDDataMat, reconMat
+    def __percentage2n(self, eigVals):
+        sortArray = np.sort(eigVals)              # 升序
+        sortArray = sortArray[-1::-1]
+        arraySum = np.sum(sortArray)
+        tempSum = 0
+        num = 0;
+        for i in sortArray:
+            tempSum += i
+            num += 1
+            if tempSum >= arraySum * self.__precison:
+                break
+        return num
 
-# Read data
-im = Image.open('lena.jpg')
-im_array = np.array(im)
-print(im_array.size)           # = row * col * channels
-# 显示图像
-im.show()
-#convert to gray scale
-im_gray = im.convert("L")
-im_gray.show()
+    def pca(self, dataIn):
+        newData, meanVals = self.__zeroMean(dataIn)
+        print(newData.shape)     # for test
+        covMat = np.cov(newData, rowvar=0)
+        eigVals, eigVects = np.linalg.eig(np.mat(covMat))    # calculate the covariance matrix
+        n = self.__percentage2n(eigVals)             # get the 'n'
+        # For Test
+        print('n = {}'.format(n))
+        index_array = np.argsort(eigVals)
+        n_eigValIndex = index_array[-1:-(n+1):-1]    # 从后面数 n 个下标
+        n_eigVects = eigVects[:, n_eigValIndex]      # 取出对应的 n 个列向量
+        lowDDataMat = newData * n_eigVects
+        reconMat = (lowDDataMat * n_eigVects.T) + meanVals
+        '''
+        index_array = np.argsort(eigVals)
+        print(index_array)     # for test
+        new_eigVals = eigVals[index_array]           # get the sorted array
+        totalEigSum = np.sum(new_eigVals)
+        tempSum = 0
+        n = 0
+        for i in range(new_eigVals.size):
+            tempSum += new_eigVals[:-i]
+            n += 1
+            if tempSum / totalEigSum > self.precison:
+                break
+        new_eigVects = eigVects[]
+        '''
+        return lowDDataMat, reconMat
 
-lowDData, reconMat = pca(im_gray, 20)
+# obtain the grayscale image
+img = ImageRead('./', 'lena.jpg')
 
-plt.axis('off')
-plt.figure("Low")
-plt.imshow(lowDData)
-plt.show()
-plt.figure("Rec")
-plt.imshow(reconMat)
-plt.show()
+# For test
+#img.show()
+
+# change the Image class data to numpy matrix class
+imgMat = np.array(img)
+'''
+imgMat = np.matrix(img.getdata(), dtype='float')
+imgMat = imgMat.reshape(img.size[0], img.size[1])
+'''
+
+mp = myPCA(0.95)
+
+decMat, resMat = mp.pca(imgMat)
+
+decImg = Image.fromarray(decMat)
+recImg = Image.fromarray(resMat)
+
+decImg.show('Decompose')
+recImg.show('Restore')
 
