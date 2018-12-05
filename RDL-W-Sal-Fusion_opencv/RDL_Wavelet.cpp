999ab0c5dc47470fb354a931e976184f8c6a6f0d
smh <song_mh@yeah.net>
2018-12-05 23:46:18 +0800

    fixed horizontal_update bugs: copyMakeBorder

--- RDL-W-Sal-Fusion_opencv/RDL_Wavelet.cpp
+++ RDL-W-Sal-Fusion_opencv/RDL_Wavelet.cpp
@@ -77,7 +77,8 @@ void RDLWavelet::Horizontal_Predict(cv::Mat &imgPre, const cv::Mat &imgIn)
 
     // Pad border
     Mat SincImg_buf(Size(SincImg.cols + 2 * Dir, SincImg.rows + 2 * Dir), CV_32F);
-    copyMakeBorder(SincImg, SincImg_buf, Dir, Dir, Dir, Dir, BORDER_REFLECT101);
+    //copyMakeBorder(SincImg, SincImg_buf, Dir, Dir, Dir, Dir, BORDER_REFLECT101);
+    copyMakeBorder(SincImg, SincImg_buf, Dir, Dir, Dir, Dir, BORDER_WRAP);
 
     // begin horizontal predict
     Mat tempMat = Mat::zeros(imgIn.size(), CV_32F);
@@ -107,19 +108,19 @@ void RDLWavelet::Horizontal_Update(cv::Mat &layerBase,const cv::Mat &layerDetail
     Mat SincImgUpdate, SincImgUpdate_buf;
     // for test
     Mat imgTest(imgIn.size(), CV_32F);
-    //resize(layerDetail, SincImgUpdate, Size(4*col, row), 4, 0, INTER_CUBIC);
     SincImgUpdate = Horizontal_SincInterpolation(layerDetail);
-    copyMakeBorder(SincImgUpdate, SincImgUpdate_buf, Dir, Dir, Dir, Dir, BORDER_REFLECT101);
+    copyMakeBorder(SincImgUpdate, SincImgUpdate_buf, Dir, Dir, Dir, Dir, BORDER_WRAP);
     float tempSum = 0.0;
-    for (int i = Dir; i < Dir + row - 1; ++i)
+    //for (int i = Dir; i < Dir + row - 1; ++i)
+    for (int i = Dir; i < Dir + row; ++i)
     {
-        auto rowPtrUp = SincImgUpdate.ptr<float>(i - 1);
-        auto rowPtrDown = SincImgUpdate.ptr<float>(i + 1);
+        auto rowPtrUp = SincImgUpdate_buf.ptr<float>(i - 1);
+        auto rowPtrDown = SincImgUpdate_buf.ptr<float>(i + 1);
         for (int j = 0; j < col; ++j)
         {
-            for (int k = -Dir; k < Dir; ++k)
+            for (int k = -Dir; k <= Dir; ++k)
                 tempSum += rowPtrUp[Dir * j + k + Dir] + rowPtrDown[Dir * j + k + Dir];
-            imgTest.at<float>(i - Dir, j) = imgIn.at<float>(i - Dir, j) + (tempSum / (Divd * 2));
+            imgTest.at<float>(i - Dir, j) = imgIn.at<float>(i - Dir, j) + (tempSum / Divd);
             tempSum = 0.0;
         }
     }
@@ -134,12 +135,11 @@ void RDLWavelet::Inverse_Horizontal_Update(cv::Mat &imgOut, const cv::Mat& imgBa
     const float Dvid = 1.0 / (2 * (Dir * 2 + 1));
     Mat tempMat, tempMat_buf;
     Mat resMat(Size(colT, rowT), imgBase.type());
-    //resize(imgDetail, tempMat, Size(Dir * colT, rowT), Dir, 0, INTER_CUBIC);
     tempMat = Horizontal_SincInterpolation(imgDetail);
-    copyMakeBorder(tempMat, tempMat_buf, Dir, Dir, Dir, Dir, BORDER_REFLECT101);
+    copyMakeBorder(tempMat, tempMat_buf, Dir, Dir, Dir, Dir, BORDER_WRAP);
 
     float tempSum = 0.0;
-    for (int i = Dir; i < Dir + rowT - 1; ++i)
+    for (int i = Dir; i < Dir + rowT; ++i)
     {
         auto rowPtrUp = tempMat_buf.ptr<float>(i - 1);
         auto rowPtrDown = tempMat_buf.ptr<float>(i + 1);
@@ -156,9 +156,6 @@ void RDLWavelet::Inverse_Horizontal_Update(cv::Mat &imgOut, const cv::Mat& imgBa
 
 void RDLWavelet::RdlWavelet(std::vector<cv::Mat> &imgOuts, const cv::Mat &imgIn)
 {
-    // test Sinc
-    Mat temp = Horizontal_SincInterpolation(imgIn);
-
     const int row = imgIn.rows;
     const int col = imgIn.cols;
     const int Dir = DIRECTION;
@@ -166,11 +163,8 @@ void RDLWavelet::RdlWavelet(std::vector<cv::Mat> &imgOuts, const cv::Mat &imgIn)
 
     Mat imgH_Detail(imgIn.size(), imgIn.type());
     Mat imgH_Base(imgIn.size(), imgIn.type());
-    Horizontal_Predict(imgH_Detail, imgIn);
+    Horizontal_Predict(imgH_Detail, imgIn);   // 他妈的是对的啊
     Horizontal_Update(imgH_Base, imgH_Detail, imgIn);
-    // for test
-    // imgOuts.push_back(imgH_Base);
-    // imgOuts.push_back(imgH_Detail);
 
     // Vertical Predict & Update
     // Part I : LL, LH
@@ -185,8 +179,8 @@ void RDLWavelet::RdlWavelet(std::vector<cv::Mat> &imgOuts, const cv::Mat &imgIn)
     // begin update
     Horizontal_Update(imgV_Base, imgV_Detail, imgV_L);   // LL
 
-    imgOuts.push_back(imgV_Base.t());
-    imgOuts.push_back(imgV_Detail.t());
+    imgOuts.push_back(imgV_Base.t());        // LL
+    imgOuts.push_back(imgV_Detail.t());      // LH
 
     // Part II : HL, HH
     Horizontal_Predict(imgV_Detail, imgV_H);
@@ -198,7 +192,7 @@ void RDLWavelet::RdlWavelet(std::vector<cv::Mat> &imgOuts, const cv::Mat &imgIn)
 
 void RDLWavelet::inverseRdlWavelet(cv::Mat &imgOut, std::vector<cv::Mat> &imgIns)
 {
-    assert(imgIns.size() == 4);    // Only includes LL, LH, HL, HH, four elements
+    assert(imgIns.size() == 4);    // Only includes LL, LH, HL, HH, four elements, i.e. A, H, V, D
     Mat imgBase(Size(col, row), imgIns[0].type());
     Mat imgDetail(Size(col, row), imgIns[0].type());
     Mat tempMat(Size(col, row), imgIns[0].type());
@@ -211,74 +205,3 @@ void RDLWavelet::inverseRdlWavelet(cv::Mat &imgOut, std::vector<cv::Mat> &imgIns
     Inverse_Horizontal_Update(imgOut, imgBase.t(), imgDetail.t());
 }
 
-/*
-void RDLWavelet(std::vector<cv::Mat> &imgOuts, const cv::Mat& imgIn)
-{
-    int row = imgIn.rows;
-    int col = imgIn.cols;
-    // 对图像进行插值
-    // In Size() constructor, the number of rows and the number of columns go in the inverse order
-    Mat SincImg = Mat::zeros(Size(4 * col, row), CV_32F);
-
-    // Interpolation basing on Sinc
-    resize(imgIn, SincImg, Size(4 * col, row), 4, 0, INTER_CUBIC);
-    // for test
-    imgOuts.push_back(SincImg);   // for test
-    for (int i = 0; i < 10; ++i)
-    {
-        for (int j = 0; j < 10; ++j)
-            cout << SincImg.at<float>(i,j) << ", ";
-        cout << endl;
-    }
-    const int Dir = 4;
-    const float Divd = 2 * ( 2 * Dir + 1 );
-
-    // Pad border
-    Mat SincImg_buf(Size(SincImg.cols + 2 * Dir, SincImg.rows + 2 * Dir), CV_32F);
-    copyMakeBorder(SincImg, SincImg_buf, Dir, Dir, Dir, Dir, BORDER_REFLECT101);
-
-    // begin horizontal predict
-    Mat tempMat = Mat::zeros(imgIn.size(), CV_32F);
-    float tempSum = 0.0;
-    cout << "Begin Horizontal Predict Success." << endl;
-    for (int i = Dir; i < Dir + row; ++i)
-    {
-        auto rowPtrUp = SincImg_buf.ptr<float>(i - 1);
-        auto rowPtrDown = SincImg_buf.ptr<float>(i + 1);
-        for (int j = 0; j < col; ++j)
-        {
-            for (int k = -Dir; k <= Dir; ++k)
-                tempSum += rowPtrUp[Dir * j + k + Dir] + rowPtrDown[Dir * j + Dir + k];
-            tempMat.at<float>(i-Dir, j) = imgIn.at<float>(i-Dir, j) - (tempSum / Divd);
-            tempSum = 0.0;
-        }
-    }
-    cout << "Horizontal Predict Success." << endl;
-    imgOuts.push_back(tempMat);
-
-    // begin Horizontal Update
-    Mat SincImgUpdate, SincImgUpdate_buf;
-    // for test
-    Mat imgTest(tempMat.size(), CV_32F);
-    resize(tempMat, SincImgUpdate, Size(4*col, row), 4, 0, INTER_CUBIC);
-    copyMakeBorder(SincImgUpdate, SincImgUpdate_buf, Dir, Dir, Dir, Dir, BORDER_REFLECT101);
-    for (int i = Dir; i < Dir + row - 1; ++i)
-    {
-        auto rowPtrUp = SincImg_buf.ptr<float>(i - 1);
-        auto rowPtrDown = SincImg_buf.ptr<float>(i + 1);
-        for (int j = 0; j < col; ++j)
-        {
-            for (int k = -Dir; k < Dir; ++k)
-                tempSum += rowPtrUp[Dir * j + k + Dir] + rowPtrDown[Dir * j + k + Dir];
-            imgTest.at<float>(i - Dir, j) = imgIn.at<float>(i - Dir, j) + (tempSum / (Divd * 2));
-            //tempMat.at<float>(i - Dir, j) = imgIn.at<float>(i - Dir, j) + (tempSum / (Divd * 2));   // for test, same as former
-            tempSum = 0.0;
-        }
-    }
-    imgOuts.push_back(imgTest);
-}
-
-void SincInterpolation(cv::Mat& imgOut, cv::Mat& imgIn)
-{
-}
-*/
